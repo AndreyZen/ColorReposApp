03aeebcf86d7737a36069edf5cb4988576748d1e
ОБПОУ КГПК <kgpk2@outlook.com>
2023-05-11 20:02:18 +0300

    part of productpage

--- ColorSetApp/Pages/ProductsPage.xaml.cs
+++ ColorSetApp/Pages/ProductsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ColorSetApp.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,49 @@ namespace ColorSetApp.Pages
         public ProductsPage()
         {
             InitializeComponent();
+        }
+
+        private void ColumnDefinition_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var a = (sender as ColumnDefinition).Parent as Grid;
+        }
+
+        private void ColumnDefinition_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void ColumnDefinition_MouseEnter1(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LVProducts.ItemsSource = App.Context.Product.ToList();
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Product product = (sender as Button).DataContext as Product;
+            product.IsActual = !product.IsActual;
+            App.Context.SaveChanges();
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddBuy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
